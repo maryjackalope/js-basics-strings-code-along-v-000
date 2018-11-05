54432ecab6328d2ec4afc45d4f6a61e10c3e364f
Mary Jack <maryjackinthebox@gmail.com>
2018-11-05 21:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage =`Welcome to Flatbook, ${currentUser}!` ;
+const welcomeMessage =`Welcome to Flatbook, !` ;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
